056aa7b833a822c8c4248351eaeb1f2c60ced781
235005 <235005@KB37-121-C05>
2025-10-21 11:37:34 +0300

    Создал базовый класс для всех предметов

--- Practic6/Program.cs
+++ Practic6/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace TextRoguelike
+{
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        public int Value { get; protected set; }
+
+        protected Item(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public abstract void Use(Player player);
+    }
+}
