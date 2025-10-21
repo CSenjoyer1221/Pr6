46e9647d469a2e0bb0e77607a6237e189bb664bb
235005 <235005@KB37-121-C05>
2025-10-21 11:51:07 +0300

    Создал класс броня

--- Practic6/Program.cs
+++ Practic6/Program.cs
@@ -34,5 +34,19 @@ namespace TextRoguelike
         }
     }
 
+    public class Armor : Item
+    {
+        public int Defense { get; private set; }
 
+        public Armor(string name, int defense, int value)
+            : base(name, value)
+        {
+            Defense = defense;
+        }
+
+        public override void Use(Player player)
+        {
+            player.EquipArmor(this);
+        }
+    }
 }
